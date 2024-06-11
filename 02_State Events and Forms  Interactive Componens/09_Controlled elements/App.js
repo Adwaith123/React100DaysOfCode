f8086143cccdcd904f4b05edadc9e3e06bbad178
Adwaith123 <zerotomastery123@gmail.com>
2024-06-11 18:59:08 +0530

    Adding files

---
+++ 02_State Events and Forms  Interactive Componens/09_Controlled elements/App.js
@@ -0,0 +1,107 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼 </h1>;
+}
+function Form() {
+  //First step in using controlled elements
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    //Clearing forms
+    setDescription("");
+    setQuantity("");
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      {/* Submit handlers on form */}
+      <h3>What do you need , for your 😍trip </h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* Here the value is coming from options value */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item...."
+        value={description} // Forcing the element to take the value of state
+        onChange={(e) => {
+          // console.log(e.target); // e.target is the html element
+          setDescription(e.target.value);
+        }} //3rd step. Here now react is controlling the state
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {/* if not returing empty object */}
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list , and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
+// Short cut for creating n array numbers of our desired size
+
+/*<select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select> */
